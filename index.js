3f18c62e6cb2a33982bcb9aa1516ab8a23adddde
imtiazovi020 <imtiazuddin018@gmail.com>
2022-02-19 11:30:43 +0600

    Staging & production environment setup

--- index.js
+++ index.js
@@ -3,22 +3,18 @@
 const http = require('http');
 const { buffer } = require('stream/consumers');
 const{handleReqRes} = require('./helpers/handleReqRes')
+const evnvironment = require('./helpers/environment')
 const routes = require('./routes')
 const {notFoundHandler} = require('./handlers/routeHandlers/notFoundHandler')
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port:3000
-};
-
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () =>{
-        console.log(`Listening to port ${app.config.port}`);
+    server.listen(evnvironment.port, () =>{
+        console.log(`Listening to port ${evnvironment.port}`);
     });
 };
 
